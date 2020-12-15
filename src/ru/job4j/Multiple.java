ac7958798e193be60bbdf78cd8c3f3a27beb8f2b
jmax2020 <jmax2020@mail.ru>
2020-12-16 00:50:47 +0700

    3.3. Разбор первой программы. [#427851]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -2,9 +2,9 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args){
-        System.out.println("1*2=2");
-        System.out.println("1*3=3");
-        System.out.println("1*9=9");
+         System.out.println("1*2=2");
+         System.out.println("1*3=3");
+         System.out.println("1*9=9");
     }
 
 }
