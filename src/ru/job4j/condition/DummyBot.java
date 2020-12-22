2d0f885e3480006656579711f65a3c22f3a4e1d0
jmax2020 <jmax2020@mail.ru>
2020-12-23 01:28:39 +0700

    4.1.2. Глупый бот [#427774]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class DummyBot {
+
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
@@ -11,11 +12,11 @@ public class DummyBot {
         return rsl;
     }
 
-
     public static void main(String[] args) {
         String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
         System.out.println(rsl);
     }
+
 }
