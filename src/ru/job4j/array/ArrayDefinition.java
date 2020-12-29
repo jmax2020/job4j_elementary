915623c8467194abfc7e5bf10baaedf9d5f8c20e
jmax2020 <jmax2020@mail.ru>
2020-12-30 00:06:54 +0700

    6.0.2. Заполнение массива. [#427812]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Max";
+        names[1] = "Petr";
+        names[2] = "Igor";
+        names[3] = "Sasha";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
