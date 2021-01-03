b64e100f4bb1ec9385730ca77b00623eab15d3d7
jmax2020 <jmax2020@mail.ru>
2021-01-04 00:00:35 +0700

    6.8. Дефрагментация массива [#427826]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                for (int j = index; j < array.length; j++) {
+                    if (array[j] != null) {
+                        int point2 = j;
+                        String temp = array[point];
+                        array[point] = array[point2];
+                        array[point2] = temp;
+                        break;
+                    }
+                }
+
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
