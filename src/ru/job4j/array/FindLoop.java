f976ee0df6e1467b0e7dadd4dc4e933fbbdb536d
jmax2020 <jmax2020@mail.ru>
2021-01-03 21:35:59 +0700

    6.5. Поиск индекса в диапазоне. [#427805]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -17,6 +17,7 @@ public class FindLoop {
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 res = i;
+                break;
             }
         }
         return  res;
