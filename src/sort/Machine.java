d1353fab077b94af2f6ca6c8d9241705eee46de6
jmax2020 <jmax2020@mail.ru>
2021-01-06 01:16:14 +0700

    6.9. Жадный алгоритм. Сдача в кофе машине [#427799]

--- src/sort/Machine.java
+++ src/sort/Machine.java
@@ -7,6 +7,15 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
+        int change = money - price;
+        for (int i = 0; i < 4; i++) {
+            while (change - coins[i] >= 0) {
+                rsl[size] = coins[i];
+                change -= coins[i];
+                size += 1;
+            }
+        }
+
         return Arrays.copyOf(rsl, size);
     }
 }
