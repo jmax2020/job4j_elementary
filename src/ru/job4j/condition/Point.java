469d0a0b97f724fb96df69becd8c25e5b471ce11
jmax2020 <jmax2020@mail.ru>
2021-01-17 23:16:05 +0700

    2.1. откат [#427987]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,22 +1,13 @@
 package ru.job4j.condition;
 
 public class Point {
-    private int x;
-    private int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double distance(Point that) {
-        return   Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    public static double distance(int x1, int y1, int x2, int y2) {
+      return   Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
-        Point one = new Point(0, 0);
-        Point two = new Point(3, 4);
-      double result = one.distance(two);
-        System.out.println("distance (0,0) to (3,4) " + result);
+      double result = Point.distance(0, 0, 2, 0);
+        System.out.println("Result (0,0) to (2,0) " + result);
     }
 }
